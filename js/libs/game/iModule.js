828ea17f18eec06c711caa5195467d6432a13045
thorin <thorin@Thorin-PC.home>
2013-01-18 13:48:24 +0100

    Object Interfaces and implementation of frame interval function

--- js/libs/game/iModule.js
+++
@@ -1,9 +0,0 @@
-/**
- * Los modulos deben de tener todos un método loadModule para ser cargados
- */
-var IModule = Class.extend({
-   sandbox: null,
-   init: function(){}, 
-   loadModule: function(){}
-});
-
